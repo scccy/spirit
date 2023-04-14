11a6ae3ffc8c66b745ae485bad7568eaeb07aae6
QingFeng2336 <331052249@qq.com>
2023-04-14 15:07:47 +0800

    uni app 基础

--- app/uniapp/spirit/pages/index/index.vue
+++ app/uniapp/spirit/pages/index/index.vue
@@ -1,9 +1,14 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+			<view class="content">
+				<textarea class="url" placeholder="输入或粘贴分享" v-model="originaladdress"></textarea>
+				<input class="uni-input" v-model="serveraddr" placeholder="服务器地址"/>
+				<input class="uni-input" v-model="serverport" placeholder="服务器端口"/>
+				<input class="uni-input" v-model="servertoken" placeholder="服务器token"/>
+				
+				<button @click="pushMessage">推送</button>
+				<button @click="saveServer">保存服务器</button>
+			</view>
 	</view>
 </template>
 
@@ -11,45 +16,111 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				originaladdress:"",
+				serveraddr:"",
+				serverport:"",
+				servertoken:""
 			}
 		},
 		onLoad() {
-
+			this.initServer();
 		},
 		onShow() {
-			
-		}
+			uni.getClipboardData({
+				success: (res) => {
+					this.originaladdress = res.data
+				}
+			})
+		},
 		methods: {
-
+			pushMessage:function(){
+				if(this.originaladdress != "" && this.serveraddr != "" && this.serverport != "" && this.servertoken != ""){
+					var api =this.serveraddr+":"+this.serverport+"/api/processingVideos";
+					var option ={
+						token:this.servertoken,
+						video:this.originaladdress
+					}
+					uni.showLoading({
+						title:"正在获取数据"
+					})
+					uni.request({
+						url: api,
+						method: "POST",
+						header: {
+							'content-type': 'application/x-www-form-urlencoded' // 默认值
+						},
+						data:option,
+						success(res) {
+							if(res.data.resCode =="000001" && res.data.message != null){
+								uni.hideLoading();
+								uni.showToast({
+									    title: res.data.message,
+									    duration: 2000,
+										icon: 'success'
+									});
+							}
+							
+						}
+					})
+					
+				}
+			},
+			saveServer:function(){
+				if(this.serveraddr != ""){
+					uni.setStorageSync('serveraddr',serveraddr)
+				}
+				if(this.serverport != ""){
+					uni.setStorageSync('serverport',serverport)
+				}
+				if(this.servertoken != ""){
+					uni.setStorageSync('servertoken',servertoken)
+				}
+				uni.showToast({
+					title: '保存成功',
+					duration: 2000
+				});
+			},
+			initServer:function(){
+				var serveraddr = uni.getStorageSync('serveraddr')
+				var serverport = uni.getStorageSync('serverport')
+				var servertoken = uni.getStorageSync('servertoken')
+				if(serveraddr != null){
+					this.serveraddr =serveraddr;
+				}
+				if(serverport != null){
+					this.serverport =serverport;
+				}
+				if(servertoken != null){
+					this.servertoken =servertoken;
+				}
+			}
 		}
 	}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+	.content{
+		width: 100%;
+		text-align: center;
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+	.url{
+		margin-top: 5%;
+		border: 1px solid #c0b9b9;
+		border-radius: 10px;
+		margin-left: 12%
 	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
+	.uni-input{
+		margin-top: 5%;
+		border: 1px solid #c0b9b9;
+		width: 80%;
+		margin-left: 12%;
+		border-radius: 10px;
 	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	button{
+		margin-top: 5%;
+		border: 1px solid #c0b9b9;
+		width: 80%;
+		margin-left: 12%;
+		border-radius: 10px;
 	}
 </style>
