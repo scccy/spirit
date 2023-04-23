61ff107e70bd037a80f1fa8f297c2a806166646e
QingFeng2336 <331052249@qq.com>
2023-04-23 20:08:45 +0800

    小程序预测试

---
+++ app/uniapp/spirit/pages/videoPlay/videoPlay.vue
@@ -0,0 +1,137 @@
+<template>
+     <view>
+		<view class="videolist">
+			<view class="videobox" v-for="item in list" @tap="palyVideo(item.videounrealaddr)">
+				<view class="video"  v-bind:style="{background:' url('+item.videocover+')',backgroundSize:'100%',backgroundRepeat:'no-repeat', backgroundPosition:'center center'}"></view>
+				<view class="title">{{item.videoname}}</view>
+				<view class="desc">{{item.videodesc}}</view>
+			</view>
+			
+		</view>
+		 
+		<view>
+			<uni-popup ref="videoPlay" type="dialog">
+				<cover-view>
+					<video id="myVideo" :src="videoPlay"  controls show-fullscreen-btn></video>
+				</cover-view>
+			</uni-popup>
+		</view>
+	 </view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: [],
+				fetchPageNum: 1,
+				videoPlay:null
+			}
+		},
+		onLoad() {
+			this.getData(this.fetchPageNum);
+		},
+		onPullDownRefresh() {
+			this.getData(this.fetchPageNum);
+		},
+		onReachBottom() {
+			this.getData(this.fetchPageNum);
+		},
+		methods: {
+			palyVideo(e){
+				this.videoPlay =e;
+				this.$refs.videoPlay.open()
+			},
+			getData(page) {
+				var that= this;
+				var option ={
+					pageNo:that.fetchPageNum
+				}
+				var serveraddr = uni.getStorageSync('serveraddr')
+				var serverport = uni.getStorageSync('serverport')
+				var servertoken = uni.getStorageSync('servertoken')
+				var api =""+serveraddr+":"+serverport+"/api/findVideos?token="+servertoken;
+				uni.request({
+					url: api,
+					method: "POST",
+					header: {
+						'content-type': 'application/x-www-form-urlencoded' // 默认值
+					},
+					data:option,
+					success(res) {
+						console.log(res);
+						if(res.data.resCode =="000001"){
+							var temp = that.list
+							that.list = [];
+							uni.showToast({
+							    icon: 'success',
+							    title: '刷新成功'
+							})
+							var content = res.data.record.content;
+							for(var  i = 0;i<content.length;i++){
+								content[i].videounrealaddr=""+serveraddr+":"+serverport+content[i].videounrealaddr
+								content[i].videocover=""+serveraddr+":"+serverport+content[i].videocover
+							}
+							that.list =temp.concat(content);
+							that.fetchPageNum=that.fetchPageNum+1;
+							console.log(that.list);
+						}
+					}
+				})
+			},
+
+		}
+	}
+</script>
+
+<style>
+	page{
+		background: #f3f3f3;
+	}
+	.grid{
+		padding-top: 10px;
+	}
+	.videobox{
+		display: inline-block;
+		text-align: center;
+		vertical-align: middle;
+		height: 15rem;
+		width: 45%;
+		float: left;
+		margin: 0.5%;
+		background-color: #fff;
+		padding: 0.5rem;
+		border-radius: 0.5rem;
+	}
+	.videobox .video{
+		height: 11rem;
+		/* width: 45%; */
+		margin: 2%;
+	}
+	.videobox .title{
+		font-size: 0.8rem;
+		text-overflow: -o-ellipsis-lastline;
+		overflow: hidden;				
+		text-overflow: ellipsis;		
+		display: -webkit-box;			
+		-webkit-line-clamp: 1;		
+		line-clamp: 1;					
+		-webkit-box-orient: vertical;	
+	}
+	.videobox .desc{
+		font-size: 0.8rem;
+		text-overflow: -o-ellipsis-lastline;
+		overflow: hidden;				
+		text-overflow: ellipsis;		
+		display: -webkit-box;			
+		-webkit-line-clamp: 2;		
+		line-clamp: 2;					
+		-webkit-box-orient: vertical;	
+	}
+	.videoPlay{
+		display: inline-block;
+		position: absolute;
+		z-index: 999;
+		top: 5%;
+	}
+</style>
--- backstage/src/main/java/com/flower/spirit/web/ApiController.java
+++ backstage/src/main/java/com/flower/spirit/web/ApiController.java
@@ -3,13 +3,17 @@ package com.flower.spirit.web;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flower.spirit.common.AjaxEntity;
 import com.flower.spirit.config.Global;
+import com.flower.spirit.entity.VideoDataEntity;
 import com.flower.spirit.service.AnalysisService;
+import com.flower.spirit.service.VideoDataService;
 
 /**
  * api 调用控制器 此处控制器不拦截  仅通过token 校验
@@ -24,6 +28,9 @@ public class ApiController {
 	private AnalysisService analysisService;
 	
 	private ExecutorService exec = Executors.newFixedThreadPool(1);
+	
+	@Autowired
+	private VideoDataService videoDataService;
 	
 	
 	/**
@@ -47,4 +54,19 @@ public class ApiController {
 	
 	}
 
+	
+	/**
+	 * app 或者小程序 分页获取视频列表功能 接口
+	 * @param req
+	 * @param res
+	 * @return
+	 */
+	@RequestMapping("/findVideos")
+	public AjaxEntity findVideos(HttpServletRequest req,VideoDataEntity res) {
+		String token = req.getParameter("token");
+		if(null == token || !token.equals(Global.apptoken)) {
+			return new AjaxEntity(Global.ajax_uri_error, "app token 错误", null);
+		}
+		return videoDataService.findPage(res);
+	}
 }
