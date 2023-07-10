8e7c8bcd9dfcd4e3689ed460a463a7ed27089931
QingFeng2336 <331052249@qq.com>
2023-07-10 11:17:16 +0800

    修改加密工具

--- backstage/src/main/java/com/flower/spirit/utils/XbogusUtil.java
+++ backstage/src/main/java/com/flower/spirit/utils/XbogusUtil.java
@@ -1,195 +1,214 @@
 package com.flower.spirit.utils;
-
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Base64;
-import java.util.Date;
 import java.util.List;
-import java.util.stream.IntStream;
-
-import org.apache.commons.codec.digest.DigestUtils;
 
 public class XbogusUtil {
+    private final Integer[] Array = {
+        null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+        null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+        null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, null, null, null, null, null, null, null, null, null, null, null,
+        null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+        null, null, null, null, null, null, null, null, null, null, null, null, 10, 11, 12, 13, 14, 15
+    };
+    private final String character = "Dkdpgh4ZKsQB80/Mfvw36XI1R25-WUAlEi7NLboqYTOPuzmFjJnryx9HVGcaStCe=";
 
-	
-	public static Integer[] array = {null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 10, 11, 12, 13, 14, 15 };
-
-	
-	public static String _0x377d66="Dkdpgh4ZKsQB80/Mfvw36XI1R25-WUAlEi7NLboqYTOPuzmFjJnryx9HVGcaStCe=";
-	
-	public static int[] _0x39ced2(String l) {
-	    int[] n = new int[l.length() / 2];
-	    for (int u = 0, i = 0; u < l.length(); ) {
-	        n[i++] = (array[l.charAt(u++)] << 4) | array[l.charAt(u++)];
-	    }
-	    return n;
-	}
-
-	public static int[] _0x1da120(String l) {
-		return _0x39ced2(MD5(intArrayToString(_0x39ced2(MD5(l)))));
-	}
-	
-	public static int _0x2efd11(char l) {
-	    String base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-	    return base64Chars.indexOf(l);
-	}
-
-	public static String decodeBase64(String l) {
-	    StringBuilder sb = new StringBuilder();
-	    for (int n = 0; n < l.length() - 3; n += 4) {
-	        int u = _0x2efd11(l.charAt(n));
-	        int e = _0x2efd11(l.charAt(n + 1));
-	        int t = _0x2efd11(l.charAt(n + 2));
-	        int r = _0x2efd11(l.charAt(n + 3));
-	        sb.append((char) ((u << 2) | (e >>> 4)));
-	        if (l.charAt(n + 2) != '=') {
-	            sb.append((char) ((e << 4 & 240) | (t >>> 2 & 15)));
-	        }
-	        if (l.charAt(n + 3) != '=') {
-	            sb.append((char) ((t << 6 & 192) | r));
-	        }
-	    }
-
-	    return sb.toString();
-	}
-
-	public static String _0x4d54ed() {
-		return null;
-	}
-	
-	public static String _0x478bb3(int l, int n, int u) {
-	    int e = (255 & l) << 16;
-	    int t = (255 & n) << 8;
-	    int r = e | t | u;
-	    return _0x377d66.charAt((16515072 & r) >> 18) + "" + _0x377d66.charAt((258048 & r) >> 12) + "" + _0x377d66.charAt((4032 & r) >> 6) + "" + _0x377d66.charAt(63 & r);
-	}
-	public static String _0x37f15d() {
-		return "";
-	}
-	
-	public static String _0x330d11(int l, int n, int u, int e, int t, int r, int o, int d, int a, int c, int i, int f, int x, int _, int h, int g, int C, int s, int p) {
-	    byte[] w = new byte[] {
-	            (byte) l, (byte) i, (byte) n, (byte) f, (byte) u, (byte) x, (byte) e, (byte) _, 
-	            (byte) t, (byte) h, (byte) r, (byte) g, (byte) o, (byte) C, (byte) d, (byte) s, 
-	            (byte) a, (byte) p, (byte) c
-	    };
-	    return new String(w, StandardCharsets.ISO_8859_1);
-	}
-
-	
-	public static String _0x330d112(String key, String data) {
-	    int[] e = new int[256];
-	    for (int i = 0; i < 256; i++) {
-	        e[i] = i;
-	    }
-
-	    int t = 0;
-	    for (int o = 0; o < 256; o++) {
-	        t = (t + e[o] + key.charAt(o % key.length())) % 256;
-	        int u = e[o];
-	        e[o] = e[t];
-	        e[t] = u;
-	    }
-
-	    t = 0;
-	    int a = 0;
-	    String r = "";
-	    for (int d = 0; d < data.length(); d++) {
-	        a = (a + 1) % 256;
-	        t = (t + e[a]) % 256;
-	        int u = e[a];
-	        e[a] = e[t];
-	        e[t] = u;
-	        r += (char) (data.charAt(d) ^ e[(e[a] + e[t]) % 256]);
-	    }
-
-	    return r;
-	}
-
-	public static String _0x33baa6(int l, int n, String u) {
-	    return Character.toString((char) l) + Character.toString((char) n) + u;
-	}
-
-	
-	
-	public static String intArrayToString(int[] arr) {
-	    StringBuilder sb = new StringBuilder();
-	    for (int i = 0; i < arr.length; i++) {
-	        sb.append(arr[i]);
-	        if (i != arr.length - 1) {
-	            sb.append(",");
-	        }
-	    }
-	    return sb.toString();
-	}
-
-	public static String getXB(String l) {
-		int[] n = _0x39ced2(MD5("d4+pTKoNjJFb5tMtAC3XB9XrDDxlig1kjbh32u+x5YcwWb/me2pvLTh6ZdBVN5skEeIaOYNixbnFK6wyJdl/Lcy9CDAcpXLLQc3QFKIDQ3KkQYie3n258eLS1YFUqFLDjn7dqCRp1jjoORamU2SV"));
-		int[] u = _0x39ced2(MD5(intArrayToString(_0x39ced2("d41d8cd98f00b204e9800998ecf8427e"))));
-		int[] e = _0x1da120(l);
-		long t = new Date().getTime() / 1000;
-		int r = 536919696;
-		int[]  o = new int[10];
-		int[]  d = new int[10];
-		String a = "";
-		int[] c = new int[]{64, (int)(0.00390625), 1, 8, e[14], e[15], u[14], u[15], n[14], n[15], (int)(t >> 24 & 255), (int)(t >> 16 & 255), (int)(t >> 8 & 255), (int)(t >> 0 & 255), (int)(r >> 24 & 255), (int)(r >> 16 & 255), (int)(r >> 8 & 255), (int)(r >> 0 & 255)};
-		int xor = c[0];
-		for (int i = 1; i < c.length; i++) {
-		    xor ^= c[i];
-		}
-		c = Arrays.copyOf(c, c.length+1);
-		c[c.length-1] = xor;
-		for (int i = 0; i < c.length; i += 2) {
-		  try {
-			  o[i]=(c[i]);
-			  d[i] =(c[i+1]);
-		} catch (Exception e2) {
-		}
-		}
-
-		int[] oarr = IntStream.concat(Arrays.stream(o), Arrays.stream(d)).toArray();
-//		char [] oarr =o.toString().toCharArray();
-		String _0x330d11 = _0x330d11(
-		    		 oarr[0],
-		    		 oarr[1],
-		    		 oarr[2],
-		    		 oarr[3],
-		    		 oarr[4],
-		    		 oarr[5],
-		    		 oarr[6],
-		    		 oarr[7],
-		    		 oarr[8],
-		    		 oarr[9],
-		    		 oarr[10],
-		    		 oarr[11],
-		    		 oarr[12],
-		    		 oarr[13],
-		    		 oarr[14],
-		    		 oarr[15],
-		    		 oarr[16],
-		    		 oarr[17],
-		    		 oarr[18]
-		    );
-	        String i = _0x33baa6(2, 255, _0x330d112(String.valueOf((char) 255), _0x330d11.substring(0, 19)));
-	        for (int j = 0; j < i.length(); ) {
-	            a += _0x478bb3(i.charAt(j++), i.charAt(j++), i.charAt(j++));
-	        }
-	        return a;
-		}
-
-		String _0x180b4c = _0x37f15d();
-
-    
-	
-    public static String MD5(String input) {
-    	return DigestUtils.md5Hex(input);
+    public Integer[] md5StrToArray(String md5Str) {
+        if (md5Str instanceof String && md5Str.length() > 32) {
+            Integer[] array = new Integer[md5Str.length()];
+            for (int i = 0; i < md5Str.length(); i++) {
+                array[i] = (int) md5Str.charAt(i);
+            }
+            return array;
+        } else {
+            List<Integer> array = new ArrayList<>();
+            int idx = 0;
+            while (idx < md5Str.length()) {
+                array.add((Array[(int) md5Str.charAt(idx)] << 4) | Array[(int) md5Str.charAt(idx + 1)]);
+                idx += 2;
+            }
+            return array.toArray(new Integer[0]);
+        }
     }
-    public static void main(String[] args) {
-    	//DFSzswVuDAxANxXhteeNrl9WX7rQ
-        String result = getXB("aweme_id=7221047525594139944&aid=6383&cookie_enabled=true&platform=PC&downlink=10");
-        System.out.println(result);
-	}
+
+    public String md5Encrypt(String urlPath) throws NoSuchAlgorithmException {
+        Integer[] hashedUrlPath = md5StrToArray(md5(md5StrToArray(md5(urlPath))));
+        StringBuilder sb = new StringBuilder();
+        for (Integer value : hashedUrlPath) {
+            sb.append(Character.toString((char) value.intValue()));
+        }
+        return sb.toString();
+    }
+
+    public String md5(Object input) throws NoSuchAlgorithmException {
+        Integer[] array;
+        if (input instanceof String) {
+            array = md5StrToArray((String) input);
+        } else if (input instanceof Integer[]) {
+            array = (Integer[]) input;
+        } else {
+            throw new IllegalArgumentException("Invalid input type. Expected String or Integer[].");
+        }
+
+        MessageDigest md5Hash = MessageDigest.getInstance("MD5");
+        byte[] bytes = new byte[array.length];
+        for (int i = 0; i < array.length; i++) {
+            bytes[i] = array[i].byteValue();
+        }
+        byte[] md5Bytes = md5Hash.digest(bytes);
+
+        StringBuilder sb = new StringBuilder();
+        for (byte b : md5Bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
+    public String encodingConversion(Integer a, Integer b, Integer c, Integer e, Integer d, Integer t, Integer f, Integer r, Integer n, Integer o, Integer i,Integer z, Integer x,
+    		Integer u, Integer s, Integer l, Integer v, Integer h, Integer p) {
+        List<Integer> y = new ArrayList<>();
+        y.add(a);
+        y.add(i);
+        y.addAll(Arrays.asList(b, z, c, x, e, u, d, s, t, l, f, v, r, h, n, p, o));
+        byte[] byteArray = new byte[y.size()];
+        for (int idx = 0; idx < y.size(); idx++) {
+            byteArray[idx] = y.get(idx).byteValue();
+        }
+        return new String(byteArray, StandardCharsets.ISO_8859_1);
+    }
+
+    public String encodingConversion2(int a, int b, String c) {
+        return Character.toString((char) a) + Character.toString((char) b) + c;
+    }
+
+    public byte[] rc4Encrypt(byte[] key, byte[] data) {
+        int[] S = new int[256];
+        int j = 0;
+        byte[] encryptedData = new byte[data.length];
+
+        // 初始化 S 盒
+        for (int i = 0; i < 256; i++) {
+            S[i] = i;
+        }
+
+        // 生成密钥流
+        for (int i = 0; i < 256; i++) {
+            j = (j + S[i] + key[i % key.length]) % 256;
+            int temp = S[i];
+            if(j>0) {
+                S[i] = S[j];
+                S[j] = temp;
+            }
+        }
+
+        // 加密数据
+        int i = 0, k = 0;
+        for (int idx = 0; idx < data.length; idx++) {
+            i = (i + 1) % 256;
+            j = (j + S[i]) % 256;
+            int temp = S[i];
+            S[i] = S[j];
+            S[j] = temp;
+            encryptedData[idx] = (byte) (data[idx] ^ S[(S[i] + S[j]) % 256]);
+        }
+
+        return encryptedData;
+    }
+
+    public String calculation(int a1, int a2, int a3) {
+        int x1 = (a1 & 255) << 16;
+        int x2 = (a2 & 255) << 8;
+        int x3 = x1 | x2 | a3;
+        return Character.toString(character.charAt((x3 & 16515072) >> 18))
+                + character.charAt((x3 & 258048) >> 12)
+                + character.charAt((x3 & 4032) >> 6)
+                + character.charAt(x3 & 63);
+    }
+
+    public String getXBogus(String urlPath) throws NoSuchAlgorithmException {
+        Integer[] array1 = md5StrToArray("d88201c9344707acde7261b158656c0e");
+        Integer[] array2 = md5StrToArray(md5(md5StrToArray("d41d8cd98f00b204e9800998ecf8427e")));
+        Integer[] urlPathArray = md5Encrypt(urlPath).chars().boxed().toArray(Integer[]::new);
+
+        int timer = (int) (System.currentTimeMillis() / 1000);
+        int ct = 536919696;
+        List<Integer> array3 = new ArrayList<>();
+        List<Integer> array4 = new ArrayList<>();
+        StringBuilder xb = new StringBuilder();
+
+        List<Object> newArray = new ArrayList<>(Arrays.asList(
+                64, 0.00390625, 1, 8,
+                urlPathArray[14], urlPathArray[15], array2[14], array2[15], array1[14], array1[15],
+                (timer >> 24) & 255, (timer >> 16) & 255, (timer >> 8) & 255, timer & 255,
+                (ct >> 24) & 255, (ct >> 16) & 255, (ct >> 8) & 255, ct & 255
+        ));
+
+        String newArrayStr =String.valueOf(newArray.get(0));
+        int xorResult = (int) Math.floor(Double.valueOf(newArrayStr));
+        for (int i = 1; i < newArray.size(); i++) {
+//            double b = (double) newArray.get(i);
+        	double b = Double.valueOf(String.valueOf(newArray.get(i)));
+            if (b % 1 == 0) {
+                xorResult ^= (int) b;
+            } else {
+                xorResult ^= (int) Math.floor(b);
+            }
+        }
+
+        newArray.add(xorResult);
+
+        for (int i = 0; i < newArray.size(); i += 2) {
+            array3.add((int) Math.floor(Double.valueOf(String.valueOf(newArray.get(i)))));
+            if (i + 1 < newArray.size()) {
+                array4.add((int) Math.floor(Double.valueOf(String.valueOf(newArray.get(i+1)))));
+            }
+        }
+
+        List<Integer> mergeArray = new ArrayList<>(array3);
+        mergeArray.addAll(array4);
+        Integer[] array5 = mergeArray.toArray(new Integer[0]);
+        String garbledCode = encodingConversion2(
+                2, 255,
+                new String(rc4Encrypt("ÿ".getBytes(StandardCharsets.ISO_8859_1),
+                        encodingConversion(array5[0],
+                        				   array5[1],
+                        				   array5[2],
+                        				   array5[3],
+                        				   array5[4],
+                        				   array5[5],
+                        				   array5[6],
+                        				   array5[7],
+                        				   array5[8],
+                        				   array5[9],
+                        				   array5[10],
+                        				   array5[11],
+                        				   array5[12],
+                        				   array5[13],
+                        				   array5[14],
+                        				   array5[15],
+                        				   array5[16],
+                        				   array5[17],
+                        				   array5[18]
+                        				).getBytes(StandardCharsets.ISO_8859_1)),
+                        StandardCharsets.ISO_8859_1)
+        );
+
+        for (int i = 0; i < garbledCode.length(); i += 3) {
+            xb.append(calculation((int) garbledCode.charAt(i), (int) garbledCode.charAt(i + 1), (int) garbledCode.charAt(i + 2)));
+        }
+
+        String params = urlPath + "&X-Bogus=" + xb.toString();
+        return params;
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String urlPath = "aweme_id=7196239141472980280&aid=1128&version_name=23.5.0&device_platform=android&os_version=2333";
+        XbogusUtil xb = new XbogusUtil();
+        String xbogus = xb.getXBogus(urlPath);
+        System.out.println(xbogus);
+    }
 }
